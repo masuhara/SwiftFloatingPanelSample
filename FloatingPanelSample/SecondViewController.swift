bb423512092eec568eaafa625ea8991562e30f96
masuhara <info@masuhara.net>
2019-06-10 10:58:30 +0900

    Completed ver 1.0

---
+++ FloatingPanelSample/SecondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondViewController.swift
+//  FloatingPanelSample
+//
+//  Created by Masuhara on 2019/06/10.
+//  Copyright © 2019 Ylab Inc. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- FloatingPanelSample/ViewController.swift
+++ FloatingPanelSample/ViewController.swift
@@ -7,14 +7,41 @@
 //
 
 import UIKit
+import FloatingPanel
 
 class ViewController: UIViewController {
+    
+    var floatingPanelController: FloatingPanelController!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setSecondViewController()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // 閉じる
+        floatingPanelController.removePanelFromParent(animated: true)
     }
 
+    func setSecondViewController() {
+        // floatingPanelを初期化
+        floatingPanelController = FloatingPanelController()
+        
+        // Main.storyboardファイルを取得
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        
+        // Storyboard上の"SecondViewController"というidentifierを付けたViewControllerを取得
+        let secondViewController = storyboard.instantiateViewController(withIdentifier: "SecondViewController") as! SecondViewController
+        
+        // floatingPanelControllerにさっき取得したViewControllerをセット
+        floatingPanelController.set(contentViewController: secondViewController)
+        floatingPanelController.addPanel(toParent: self, belowView: nil, animated: true)
+    }
+    
+    
 
 }
 
