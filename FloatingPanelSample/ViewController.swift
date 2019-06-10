b761bb146a109bda1c9cae693fc9fdc71c775261
masuhara <info@masuhara.net>
2019-06-10 10:23:17 +0900

    Initial Commit

---
+++ FloatingPanelSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FloatingPanelSample
+//
+//  Created by Masuhara on 2019/06/10.
+//  Copyright © 2019 Ylab Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
